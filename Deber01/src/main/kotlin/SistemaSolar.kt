51aef5b9ea0b924b64fb39c45a5b142e060e79e4
Elisa Herrera <andrea.herrera@epn.edu.ec>
2023-06-17 00:01:33 -0500

    terminado

--- Deber01/src/main/kotlin/SistemaSolar.kt
+++ Deber01/src/main/kotlin/SistemaSolar.kt
@@ -1,42 +1,16 @@
-class SistemaSolar(
-    var edad: Int,
-    var nombre: String,
+class SistemaSolar {
+    var edad: Int? = 0;
+    var nombre: String =" ";
+    var tipo: String  =" ";
+    var galaxia: String =" ";
+    var distancia: Double  =0.0
 
-    var tipo: String,
-    var galaxia: String
-) {
-    var planetas: MutableList<Planeta> = mutableListOf()
-
-    fun agregarPlaneta(planeta: Planeta) {
-        planetas.add(planeta)
+    constructor(edad: Int?, nombre:String, tipo:String, galaxia:String,distancia:Double) { // Bloque de codigo del constructor
+        this.edad =  edad
+        this.nombre =nombre
+        this.tipo = tipo
+        this.galaxia = galaxia
+        this.distancia = distancia
     }
-
-    fun mostrarPlanetas() {
-        if (planetas.isEmpty()) {
-            println("El sistema solar no contiene planetas.")
-        } else {
-            println("Edad: $edad años")
-            println("Sistema Solar: $nombre")
-
-            println("Tipo: $tipo")
-            println("Galaxia: $galaxia")
-            println()
-            for (planeta in planetas) {
-                println("ID: ${planeta.idPlaneta}")
-                println("Nombre: ${planeta.nombrePlaneta}")
-                println("Número de Lunas: ${planeta.numeroLunas}")
-                println("Diámetro: ${planeta.diametro}")
-                println("Masa: ${planeta.masa}")
-                println("Distancia media al Sol: ${planeta.distanciaMediaSol}")
-                println()
-            }
-        }
-    }
-
-    companion object {
-        fun agregarPlaneta(planeta: Planeta) {
-            TODO("Not yet implemented")
-        }
-    }
-
 }
+
