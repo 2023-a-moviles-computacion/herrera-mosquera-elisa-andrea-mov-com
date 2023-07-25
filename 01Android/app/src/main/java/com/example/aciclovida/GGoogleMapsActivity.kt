2caf420d50643527bb185ad2b14b9875c6a433fc
Elisa Herrera <andrea.herrera@epn.edu.ec>
2023-07-25 10:13:06 -0500

    Clase20

--- 01Android/app/src/main/java/com/example/aciclovida/GGoogleMapsActivity.kt
+++ 01Android/app/src/main/java/com/example/aciclovida/GGoogleMapsActivity.kt
@@ -3,6 +3,8 @@ package com.example.aciclovida
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PackageManagerCompat
@@ -20,7 +22,20 @@ class GGoogleMapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ggoogle_maps)
         solicitarPermisos()
         iniciarLogicaMapa()
+        val boton = findViewById<Button>(R.id.btn_ir_carolina)
+        boton.
+                setOnClickListener{
+                    irCarolina()
+                }
     }
+    fun irCarolina(){
+        val carolina = LatLng(-0.1825684318486696, -78.48447277600916)
+        val zoom = 17f
+        moverCamaraConZoom(carolina, zoom)
+
+    }
+
+    
     fun solicitarPermisos(){
         val contexto = this.applicationContext
         val nombrePermiso = android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -55,10 +70,42 @@ class GGoogleMapsActivity : AppCompatActivity() {
                 moverQuicentro()
                 anadirPolilinea()
                 anadirPoligono()
+                escucharListeners()
             }
 
         }
     }
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+            it.tag//ID
+        }
+        mapa.setOnPolylineClickListener{
+            Log.i("mapa", "setOnPolyLineClickListener ${it}")
+            it.tag//ID
+        }
+       mapa.setOnMarkerClickListener {
+           Log.i("mapa","setOnMarkerClickListener ${it}")
+           it.tag//id
+           return@setOnMarkerClickListener true
+       }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraStartedListener ${it} ")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdListener")
+        }
+    }
+
+
 
     fun anadirPolilinea(){
         with(mapa){
